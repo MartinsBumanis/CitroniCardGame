f56596d454047caa7accad9f5c036a5839b7726e
Martins Bumanis <bumanis.martins@gmail.com>
2020-12-02 20:02:08 +0200

    port changed

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 require("./cardEngineServer");
 
-const http = require('http')
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! NodeJS \n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
 const express = require("express");
 const expressSession = require("express-session");
+var https = require('https');
 const path = require("path");
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
